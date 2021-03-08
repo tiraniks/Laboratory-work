5f1926867396195159a080475b0b9dec759a1579
tiraniks <53484510+tiraniks@users.noreply.github.com>
2021-03-08 21:07:45 +0200

    Add files via upload

---
+++ Lab5T1/Lab5T1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab5T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
