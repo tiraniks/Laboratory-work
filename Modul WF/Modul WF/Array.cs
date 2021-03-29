8f4d3385359ca2f14fbe1d1e7f4b6313a4541555
tiraniks <53484510+tiraniks@users.noreply.github.com>
2021-03-29 12:53:40 +0300

    Add files via upload

---
+++ Modul WF/Modul WF/Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_WF
+{
+    class Array
+    {
+        private double Construct;
+        //Конструктор без параметра
+        public Array()
+        {
+            this.Construct = 0;
+        }
+        //Конструктор з параметром
+        public Array(double Arr)
+        {
+            this.Construct = Arr;
+        }
+        //Конструктор копіювання
+        public Array(Array array)
+        {
+            this.Construct = array.Construct;
+        }
+
+
+
+    }
+}
---
+++ Modul WF/Modul WF/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modul_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+
+            double[] ay = new double[n];
+
+            string[] ar = textBox2.Text.Split(' ');
+            double[] z = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                z[i] = Convert.ToDouble(ar[i]);
+            }
+
+            Array.Sort(ar);
+            dataGridView2.RowCount = 1;
+            dataGridView2.ColumnCount = ar.Length;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                dataGridView2.Rows[0].Cells[i].Value = ar[i];
+            }
+
+
+
+            double a = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if (a < z[i])
+                {
+                    a = z[i];
+                }
+
+            }
+
+            textBox3.Text = a.ToString();
+
+
+            double min = 1000;
+            for (int j = 0; j < n; j++)
+            {
+                ay[j] = Convert.ToDouble(ar[j]);
+            }
+
+            for (int i = 0; i < n; i += 1)
+            {
+                if (min > ay[i])
+                {
+                    min = ay[i];
+                }
+            }
+
+            textBox4.Text = min.ToString();
+
+
+           
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+
+            double[] ay = new double[n];
+
+            string[] ar = textBox2.Text.Split(' ');
+            double[] z = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                z[i] = Convert.ToDouble(ar[i]);
+
+                if (z[i] % 3 == 0)
+                {
+                    dataGridView1.Rows[0].Cells[i].Value = z[i];
+                }
+                else
+                {
+                    textBox7.Text = $"Чисел кратних 3 нема";
+                }
+            }
+
+            
+
+        }
+    }   
+}
+    
+
