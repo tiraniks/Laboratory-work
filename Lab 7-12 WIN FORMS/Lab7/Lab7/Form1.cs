7b95a595c181f78782bb002c6575458260b851d6
tiraniks <53484510+tiraniks@users.noreply.github.com>
2021-04-05 22:47:26 +0300

    Add files via upload

---
+++ Lab 7-12 WIN FORMS/Lab7/Lab7/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        { 
+           int n = Convert.ToInt32(textBox1.Text);
+
+            Matrix tmx = new Matrix(n);
+            int[,] matrix = tmx.InputMatrix();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+
+            Convert.ToString(Matrix.Min());
+
+            Convert.ToString(Matrix.Max());
+
+        }
+
+
+    }
+}
---
+++ Lab 7-12 WIN FORMS/Lab7/Lab7/Matrix.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Matrix
+    {
+        static public int n;
+
+        int b = n;
+
+        public Matrix(){}
+
+        public Matrix(int b)
+        {
+            this.b = b;
+        }     
+
+        int[,] matrix = new int[n, n];
+
+        public Matrix(int[,] matrix)
+        {
+
+        }
+
+        public int[,] Print()
+        {
+            return matrix;
+        }
+
+        
+
+        Random rand = new Random(DateTime.Now.Millisecond);
+
+        //Введення матриці
+        public int[,] InputMatrix()
+        {
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rand.Next(-100, 100);
+                }
+            }
+
+            return matrix;
+
+        }
+        
+        public int Min()
+        {
+            int a = matrix[0, 0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] < a)
+                    {
+                        a = matrix[i, j];
+                    }
+                }
+            }
+            return a;
+
+           
+        }
+
+        public int Max()
+        {
+            int a = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (a < matrix[i, j])
+                    {
+                        a = matrix[i, j];
+                    }
+                }
+            }
+            return a;
+        }
+    }
+}    
