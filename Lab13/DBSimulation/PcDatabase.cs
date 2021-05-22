54d6f54b5ef933c3862b7b40f6aba93a06c67cda
tiraniks <53484510+tiraniks@users.noreply.github.com>
2021-05-22 16:04:06 +0300

    Add files via upload

---
+++ Lab13/DBSimulation/PcDatabase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBSimulation
+{
+    class PcDatabase
+    {
+        private List<PC> _Db = new List<PC>();
+
+        //додати
+        public void Add(PC Pc)
+        {
+            _Db.Add(Pc);
+        }
+
+        //вилучити
+        public void Remove(PC Pc)
+        {
+            _Db.Remove(Pc);
+        }
+
+        //дізнатись кількість записів в базі
+        public int Count
+        {
+            get
+            {
+                return _Db.Count;
+            }
+        }
+
+        public List<PC> Query(Func<PC, bool>  condition)
+        {
+            return _Db.Where(condition).ToList();
+        }
+
+        public List<PC> GetAll()
+        {
+            return this._Db;
+        }
+
+        public PC GetByIndex(int index)
+        {
+            return this._Db[index];
+        }
+    }
+}
